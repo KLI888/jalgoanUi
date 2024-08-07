e0888ad3751668d1267d9b7ba9f3b9e18b8c05a3
Your Name <krushnaborase40@gmail.com>
2024-08-07 19:54:24 +0530

    kriss

--- src/components/SpecialSections/SpecialSections.jsx
+++ src/components/SpecialSections/SpecialSections.jsx
@@ -3,48 +3,48 @@ import { Link } from 'react-router-dom';
 import './SpecialSections.css';
 import { FormContext } from '../../context/FormContext';
 function SpecialSections() {
-  const {closeSpecial} = useContext(FormContext)
+  const { closeSpecial } = useContext(FormContext)
 
   return (
     <>
-    <div className="special_section">
-      <div className="special_section_btns">
-        <Link to='/'>
-          <div className="special_btn btn_ngo">NGO</div>
-        </Link>
-        <Link to='/'>
-          <div className="special_btn btn_directory">Directory</div>
-        </Link>
-        <Link to='/'>
-          <div className="special_btn btn_freeList">Free Listing</div>
-        </Link>
-        <Link to='/'>
-          <div className="special_btn btn_events">Events</div>
-        </Link>
+      <div className="special_section">
+        <div className="special_section_btns">
+          <a href='https://jalgaon.gov.in/public-utility-category/ngos/' target='_blank'>
+            <div className="special_btn btn_ngo">NGO</div>
+          </a>
+          <a href='https://jalgaon.gov.in/telephone/' target='_blank'>
+            <div className="special_btn btn_directory">Directory</div>
+          </a>
+          <a href=' https://jalgaon.gov.in/tourist-places/' target='_blank'>
+            <div className="special_btn btn_freeList">Tourist places</div>
+          </a>
+          <Link to='/'>
+            <div className="special_btn btn_events">Events</div>
+          </Link>
+        </div>
       </div>
-    </div>
 
-    {/* special_section_two */}
-    <div className={`special_section_two ${closeSpecial ? '' : 'active'}`}>
-      <div className="sepcial_section_btns_two">
-        <ul>
-        <Link to='/'>
-          <div className="special_btn_two btn_ngo">NGO</div>
-        </Link>
-        <Link to='/'>
-          <div className="special_btn_two btn_directory">Directory</div>
-        </Link>
-        <Link to='/'>
-          <div className="special_btn_two btn_freeList">Free Listing</div>
-        </Link>
-        <Link to='/'>
-          <div className="special_btn_two btn_events">Events</div>
-        </Link>
-        </ul>
+      {/* special_section_two */}
+      <div className={`special_section_two ${closeSpecial ? '' : 'active'}`}>
+        <div className="sepcial_section_btns_two">
+          <ul>
+            <a href='https://jalgaon.gov.in/public-utility-category/ngos/' target='_blank'>
+              <div className="special_btn_two btn_ngo">NGO</div>
+            </a>
+            <a href='https://jalgaon.gov.in/telephone/' target='_blank'>
+              <div className="special_btn_two btn_directory">Directory</div>
+            </a>
+            <a href=' https://jalgaon.gov.in/tourist-places/' target='_blank'>
+              <div className="special_btn_two btn_freeList">Tourist places</div>
+            </a>
+            <Link to='/'>
+              <div className="special_btn_two btn_events">Events</div>
+            </Link>
+          </ul>
+        </div>
       </div>
-    </div>
     </>
-    
+
   );
 }
 
