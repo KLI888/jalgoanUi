223c4c08ee5c0e8b961ca107770fb141badc92b5
Your Name <krushnaborase40@gmail.com>
2024-08-09 10:21:51 +0530

    kjriss

--- src/components/Bottomnav/Bottomnav.jsx
+++ src/components/Bottomnav/Bottomnav.jsx
@@ -16,55 +16,55 @@ function Bottomnav() {
   return (
     <div className="bottom_nav">
       <div className="bottom_nav_links">
-        <a
-          href="#"
+        <NavLink
+          to="/"
           className={`bottom_nav_links ${activeLink === 'home' ? 'active' : ''}`}
           onClick={() => handleLinkClick('home')}
         >
           <i className='bx bx-home'></i>
           <p>Home</p>
-        </a>
-        <Link
-          to="/categories"
+        </NavLink>
+        <NavLink
+          to="/advertise"
           className={`bottom_nav_links ${activeLink === 'directory' ? 'active' : ''}`}
           onClick={() => handleLinkClick('directory')}
         >
           <i className='bx bx-folder'></i>
           <p>Directory</p>
-        </Link>
-        <a
-          href="#"
+        </NavLink>
+        <NavLink
+          to="/addListig"
           className={`bottom_nav_links ${activeLink === 'add_listing' ? 'active' : ''}`}
           onClick={() => handleLinkClick('add_listing')}
         >
           <i className='bx bx-plus-circle'></i>
           <p>Add Listing</p>
-        </a>
-        <a
-          href="#"
+        </NavLink>
+        <NavLink
+          to="/"
           className={`bottom_nav_links ${activeLink === 'events' ? 'active' : ''}`}
           onClick={() => handleLinkClick('events')}
         >
           <i className='bx bx-calendar'></i>
-          <p>Events</p>
-        </a>
+          <p>News</p>
+        </NavLink>
 
         {user ? (
           <>
-            <Link
+            <NavLink
               to="/account"
               className={`bottom_nav_links ${activeLink === 'account' ? 'active' : ''}`}
               onClick={() => handleLinkClick('account')}
             >
               <i className='bx bx-user-circle'></i>
               <p>Account</p>
-            </Link>
+            </NavLink>
           </>
         ) : (
-          <Link onClick={() => setCloseForm(!closeForm)} className="bottom_nav_links">
+          <NavLink onClick={() => setCloseForm(!closeForm)} className="bottom_nav_links">
             <i className='bx bx-user-circle'></i>
             <p>Account</p>
-          </Link>
+          </NavLink>
         )}
         {/* <Link
           to="/account"
