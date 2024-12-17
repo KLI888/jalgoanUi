113ac41b74bc08dd2bb165682472b5eb8751ec3b
Krishna Borse <125799973+KLI888@users.noreply.github.com>
2024-12-17 22:44:13 +0530

    Update Live.jsx

--- src/components/LiveStream/Live.jsx
+++ src/components/LiveStream/Live.jsx
@@ -1,53 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import { useParams, useLocation } from 'react-router-dom';
 import './Live.css';
 
 function Live() {
   const { id } = useParams();
-  const apiUrl = `${import.meta.env.VITE_DJANGO_API}/app/live/`;
-  const [ads, setAds] = useState({});
-  const [csrfToken, setCsrfToken] = useState('');
+  const location = useLocation();  // Get the location object
+  const [ads, setAds] = useState(location.state?.itemData || {}); // Use the passed data
 
-  // Fetch CSRF token
-  const getCsrfToken = async () => {
-    try {
-      const response = await axios.get(`${import.meta.env.VITE_DJANGO_API}/app/csrf-token/`);
-      setCsrfToken(response.data.csrfToken);
-    } catch (error) {
-      console.error('Error fetching CSRF token:', error);
-    }
-  };
-
-  // Fetch ads data when component mounts
-  useEffect(() => {
-    getCsrfToken(); // Fetch CSRF token first
-  
-    const fetchAdsData = async () => {
-      try {
-        console.log("Fetching ads data with token", csrfToken);
-        const response = await axios.get(apiUrl, {
-          headers: {
-            'X-CSRFToken': csrfToken,
-            'Authorization': `Bearer ${localStorage.getItem('token')}`,
-          },
-          params: { id: id },
-        });
-        setAds(response.data); // Set fetched ads data
-        console.log(response.data); // For debugging
-      } catch (error) {
-        console.error('Error fetching ads data:', error.response?.data || error);
-      }
-    };
-  
-    if (csrfToken) {
-      fetchAdsData(); // Fetch ads only after CSRF token is available
-    }
-  }, []); // Rerun effect when csrfToken or id changes
-  // Rerun effect when csrfToken or id changes
-
-  if (!ads.crousel_add_img_code) {
-    return <div className="iframe-container">Loading...</div>; // Loading state if ad data is not available
+  // If data is not available, show a loading message or return early
+  if (!ads || !ads.crousel_add_img_code) {
+    return <div className="iframe-container">Loading...</div>;
   }
 
   return (
