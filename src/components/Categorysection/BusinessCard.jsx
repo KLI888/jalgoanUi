ebd84a38a6efd4be37fde7f0de913fa1e09586b8
Your Name <krushnaborase40@gmail.com>
2025-01-09 18:18:07 +0530

    kriss

--- src/components/Categorysection/BusinessCard.jsx
+++ src/components/Categorysection/BusinessCard.jsx
@@ -43,6 +43,7 @@ function BusinessCard({ businessData, is_like, is_edit=false }) {
         <div className="business_card">
             <div className="business_imgg">
                 <img src={`${img_url}${businessData.business_banner}`} alt="" />
+                <img src={`${businessData.business_banner}`} alt="" />
             </div>
             <div className="business_info">
                 <p className='business_name'>
