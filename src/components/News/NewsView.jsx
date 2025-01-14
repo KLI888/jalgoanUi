a981caa257f5eb28529c7fedcc4daa9bcb16799f
Your Name <krushnaborase40@gmail.com>
2025-01-14 19:42:13 +0530

    kriss

--- src/components/News/NewsView.jsx
+++ src/components/News/NewsView.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import './NewsView.css';
 const NewsView = () => {
   const djangoApi = import.meta.env.VITE_DJANGO_API;
